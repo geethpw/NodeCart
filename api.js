a44ad6aa9cc069085c9e9fb66fc0958a3e6553cf
Geeth <jgpwick@live.com>
2015-12-02 12:04:30 +0530

    adding user model

--- api.js
+++ api.js
@@ -1,31 +1,45 @@
+var bodyparser = require('body-parser');
 var express = require('express');
 var status = require('http-status');
 
 module.exports = function(wagner) {
   var api = express.Router();
 
-  api.get('/product/id/:id', wagner.invoke(function(Product) {
-    return function(req, res) {
-      Product.findOne({ _id: req.params.id },
-        handleOne.bind(null, 'product', res));
-    };
-  }));
+  api.use(bodyparser.json());
 
-  api.get('/product/category/:id', wagner.invoke(function(Product) {
+  api.put('/me/cart', wagner.invoke(function(User) {
     return function(req, res) {
-      var sort = { name: 1 };
-      if (req.query.price === "1") {
-        sort = { 'internal.approximatePriceUSD': 1 };
-      } else if (req.query.price === "-1") {
-        sort = { 'internal.approximatePriceUSD': -1 };
+      try {
+        var cart = req.body.data.cart;
+      } catch(e) {
+        return res.
+          status(status.BAD_REQUEST).
+          json({ error: 'No cart specified!' });
       }
 
-      Product.
-        find({ 'category.ancestors': req.params.id }).
-        sort(sort).
-        exec(handleMany.bind(null, 'products', res));
+      req.user.data.cart = cart;
+      req.user.save(function(error, user) {
+        if (error) {
+          return res.
+            status(status.INTERNAL_SERVER_ERROR).
+            json({ error: error.toString() });
+        }
+        return res.json({ user: user });
+      });
     };
   }));
+
+  api.get('/me', function(req, res) {
+    if (!req.user) {
+      return res.
+        status(status.UNAUTHORIZED).
+        json({ error: 'Not logged in' });
+    }
+
+    req.user.populate(
+      { path: 'data.cart.product', model: 'Product' },
+      handleOne.bind(null, 'user', res));
+  });
 
   return api;
 };
@@ -40,18 +54,6 @@ function handleOne(property, res, error, result) {
     return res.
       status(status.NOT_FOUND).
       json({ error: 'Not found' });
-  }
-
-  var json = {};
-  json[property] = result;
-  res.json(json);
-}
-
-function handleMany(property, res, error, result) {
-  if (error) {
-    return res.
-      status(status.INTERNAL_SERVER_ERROR).
-      json({ error: error.toString() });
   }
 
   var json = {};
--- test.js
+++ test.js
@@ -1,27 +1,40 @@
 var assert = require('assert');
 var express = require('express');
+var status = require('http-status');
 var superagent = require('superagent');
 var wagner = require('wagner-core');
 
 var URL_ROOT = 'http://localhost:3000';
+var PRODUCT_ID = '000000000000000000000001';
 
-describe('Category API', function() {
+describe('Product API', function() {
   var server;
   var Category;
   var Product;
+  var User;
 
   before(function() {
     var app = express();
 
     // Bootstrap server
     models = require('./models')(wagner);
-    app.use(require('./api')(wagner));
-
-    server = app.listen(3000);
 
     // Make models available in tests
     Category = models.Category;
     Product = models.Product;
+    User = models.User;
+
+    app.use(function(req, res, next) {
+      User.findOne({}, function(error, user) {
+        assert.ifError(error);
+        req.user = user;
+        next();
+      });
+    });
+
+    app.use(require('./api')(wagner));
+
+    server = app.listen(3000);
   });
 
   after(function() {
@@ -35,43 +48,15 @@ describe('Category API', function() {
       assert.ifError(error);
       Product.remove({}, function(error) {
         assert.ifError(error);
-        done();
-      });
-    });
-  });
-
-  it('can load a product by id', function(done) {
-    // Create a single product
-    var PRODUCT_ID = '000000000000000000000001';
-    var product = {
-      name: 'LG G4',
-      _id: PRODUCT_ID,
-      price: {
-        amount: 300,
-        currency: 'USD'
-      }
-    };
-    Product.create(product, function(error, doc) {
-      assert.ifError(error);
-      var url = URL_ROOT + '/product/id/' + PRODUCT_ID;
-      // Make an HTTP request to
-      // "localhost:3000/product/id/000000000000000000000001"
-      superagent.get(url, function(error, res) {
-        assert.ifError(error);
-        var result;
-        // And make sure we got the LG G4 back
-        assert.doesNotThrow(function() {
-          result = JSON.parse(res.text);
+        User.remove({}, function(error) {
+          assert.ifError(error);
+          done();
         });
-        assert.ok(result.product);
-        assert.equal(result.product._id, PRODUCT_ID);
-        assert.equal(result.product.name, 'LG G4');
-        done();
       });
     });
   });
 
-  it('can load all products in a category with sub-categories', function(done) {
+  beforeEach(function(done) {
     var categories = [
       { _id: 'Electronics' },
       { _id: 'Phones', parent: 'Electronics' },
@@ -89,6 +74,7 @@ describe('Category API', function() {
         }
       },
       {
+        _id: PRODUCT_ID,
         name: 'Asus Zenbook Prime',
         category: { _id: 'Laptops', ancestors: ['Electronics', 'Laptops'] },
         price: {
@@ -106,39 +92,72 @@ describe('Category API', function() {
       }
     ];
 
-    // Create 4 categories
-    Category.create(categories, function(error, categories) {
+    var users = [{
+      profile: {
+        username: 'vkarpov15',
+        picture: 'http://pbs.twimg.com/profile_images/550304223036854272/Wwmwuh2t.png'
+      },
+      data: {
+        oauth: 'invalid',
+        cart: []
+      }
+    }];
+
+    Category.create(categories, function(error) {
       assert.ifError(error);
-      // And 3 products
-      Product.create(products, function(error, products) {
+      Product.create(products, function(error) {
         assert.ifError(error);
-        var url = URL_ROOT + '/product/category/Electronics';
-        // Make an HTTP request to localhost:3000/product/ancestor/Electronics
+        User.create(users, function(error) {
+          assert.ifError(error);
+          done();
+        });
+      });
+    });
+  });
+
+  it('can save users cart', function(done) {
+    var url = URL_ROOT + '/me/cart';
+    superagent.
+      put(url).
+      send({
+        data: {
+          cart: [{ product: PRODUCT_ID, quantity: 1 }]
+        }
+      }).
+      end(function(error, res) {
+        assert.ifError(error);
+        assert.equal(res.status, status.OK);
+        User.findOne({}, function(error, user) {
+          assert.ifError(error);
+          assert.equal(user.data.cart.length, 1);
+          assert.equal(user.data.cart[0].product, PRODUCT_ID);
+          assert.equal(user.data.cart[0].quantity, 1);
+          done();
+        });
+      });
+  });
+
+  it('can load users cart', function(done) {
+    var url = URL_ROOT + '/me';
+
+    User.findOne({}, function(error, user) {
+      assert.ifError(error);
+      user.data.cart = [{ product: PRODUCT_ID, quantity: 1 }];
+      user.save(function(error) {
+        assert.ifError(error);
+
         superagent.get(url, function(error, res) {
           assert.ifError(error);
+
+          assert.equal(res.status, 200);
           var result;
           assert.doesNotThrow(function() {
-            result = JSON.parse(res.text);
+            result = JSON.parse(res.text).user;
           });
-          assert.equal(result.products.length, 2);
-          // Should be in ascending order by name
-          assert.equal(result.products[0].name, 'Asus Zenbook Prime');
-          assert.equal(result.products[1].name, 'LG G4');
-
-          // Sort by price, ascending
-          var url = URL_ROOT + '/product/category/Electronics?price=1';
-          superagent.get(url, function(error, res) {
-            assert.ifError(error);
-            var result;
-            assert.doesNotThrow(function() {
-              result = JSON.parse(res.text);
-            });
-            assert.equal(result.products.length, 2);
-            // Should be in ascending order by name
-            assert.equal(result.products[0].name, 'LG G4');
-            assert.equal(result.products[1].name, 'Asus Zenbook Prime');
-            done();
-          });
+          assert.equal(result.data.cart.length, 1);
+          assert.equal(result.data.cart[0].product.name, 'Asus Zenbook Prime');
+          assert.equal(result.data.cart[0].quantity, 1);
+          done();
         });
       });
     });
